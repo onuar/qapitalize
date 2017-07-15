b405a4d5619f935168408ab060154af2cc031171
Onur Aykac <onuraykac@gmail.com>
2017-07-15 11:54:33 +0700

    Random array element

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,12 @@
 import * as data from './resource.json';
 
 export function it(param: string):string{
-    console.log((<any>data));
+    let length = (<any>data).length;
+    let max = Math.floor(length);
+    let index = Math.floor(Math.random() * (max - 0)) + 0;
+    
+    console.log((<any>data)[index]);
+    // console.log((<any>data));
     return param;
 }
 
